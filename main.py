cd00ba06d1517b3e611dc51be25f6a4b8ec470fb
notnotype <2056369669@qq.com>
2020-12-19 21:16:51 +0800

    update: main.py

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ general_template
 """
 
 import logging
+from typing import List
 
 from peewee import *
 
@@ -48,31 +49,63 @@ class Comment(Model):
         database = db
 
 
-def get_comment(resp: spider.Response):
-    # 包装 comment
+def parse_comment(resp: spider.Response) -> List[Comment]:
+    """获取 'http://www.dianping.com/shop/l9ZszA41xUchPAwb' 的评论
+
+    :param resp: http://www.dianping.com/shop/l9ZszA41xUchPAwb
+    :return: List[Comment]
+    """
+    # 包装 comments
     return []
 
 
-def get_info(url: str):
+def parse_info(url: str):
+    """直接解析 https://github.com/notnotype/dianping/invitations 包括评论
+
+    :param url: like https://github.com/notnotype/dianping/invitations
+    :return: None
+    """
     # 包装post表
+    # 请求url
     resp = spider.get(url)
     title = resp.css('title')
     title = resp.xpath('//title/test()')
     title = title[0].text
 
-    # 包装
+    # 包装 Shop
     shop = Shop()
     shop.url = url
     shop.tel = '45465-654564'
     # 包装评论
-    comments = get_comment(resp)
-    ...
+    comments = parse_comment(resp)
+    for comment in comments:
+        comment.save()
+
+    # 在包装后
+    shop.save()
 
 
-def search(keyword, position):
-    """生成器返回搜索页面的url"""
+def search(keyword: str, position: str):
+    """生成器返回搜索页面的url每一页
+
+    :param keyword:
+    :param position:
+    :return:
+    """
     yield ''
     ...
+
+
+def shop_page_generator(keyword: str, position: str) -> list:
+    """生成店铺页面url
+
+    :param keyword:
+    :param position: 地点
+    :return: url like http://www.dianping.com/shop/l9ZszA41xUchPAwb
+    """
+    for search_page in search(keyword, position):
+        # 解析获得 'http://www.dianping.com/shop/l9ZszA41xUchPAwb'等url
+        yield 'http://www.dianping.com/shop/l9ZszA41xUchPAwb'
 
 
 def main():
@@ -87,8 +120,17 @@ def main():
     keyword = input('input keyword: ')
     position = input('input position: ')
 
-    for search_url in search(keyword, position):
-        get_info(search_url)
+    for search_url in shop_page_generator(keyword, position):
+        '''search_url = *
+        [
+        'http://www.dianping.com/shop/l9ZszA41xUchPAwb',
+        'http://www.dianping.com/shop/l9ZszA41xUchPAwb',
+        'http://www.dianping.com/shop/l9ZszA41xUchPAwb',
+        'http://www.dianping.com/shop/l9ZszA41xUchPAwb',
+        ]
+        '''
+        # 解析保存数据
+        parse_info(search_url)
 
 
 if __name__ == '__main__':
